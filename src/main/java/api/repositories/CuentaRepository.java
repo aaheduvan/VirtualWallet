eca0dc1e2af1b34990f8460847b5e36c51a2d58c
Andres Heduvan <aaheduvan@gmail.com>
2020-12-21 17:54:09 -0300

    Fix Min/Max Balance de Cuentas

--- src/main/java/api/repositories/CuentaRepository.java
+++ src/main/java/api/repositories/CuentaRepository.java
@@ -13,12 +13,8 @@ public interface CuentaRepository extends CrudRepository<Cuenta, Integer> {
 	boolean existsByAlias(String alias);
 	List<Cuenta> findByidCliente(Long idCliente);
 	List<Cuenta> findByMoneda(String moneda);
-	@Query(value = "SELECT *" + 
-					"FROM Cuenta" + 
-					"WHERE balance=(SELECT MAX(balance) FROM cuenta);", nativeQuery = true)
+	@Query(value = "SELECT * FROM cuenta WHERE balance = (SELECT max(balance) FROM cuenta)", nativeQuery = true)
 	Optional<Cuenta> findMaxBalance();
-	@Query(value = "SELECT *" + 
-			"FROM Cuenta" + 
-			"WHERE balance=(SELECT MIN(balance) FROM cuenta);", nativeQuery = true)
+	@Query(value = "SELECT * FROM cuenta WHERE balance=(SELECT min(balance) FROM cuenta)", nativeQuery = true)
 	Optional<Cuenta> findMinBalance();
 }
